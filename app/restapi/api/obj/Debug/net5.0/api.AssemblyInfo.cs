cae4afd92e40c58b6c73fad2295d3c5699b86737
Lumbar <eduardojgm_@hotmail.com>
2021-03-18 00:45:55 +0100

    DockerFile

--- app/restapi/api/obj/Debug/net5.0/api.AssemblyInfo.cs
+++ app/restapi/api/obj/Debug/net5.0/api.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1937d8f1-d9b1-41ef-b120-695a22710ffc")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
